242a75c1f61690f6d5396c3bacf8a1d6d44d7642
huyiwen <1020030101@qq.com>
2024-03-30 10:22:56 +0800

    Remove port

--- routers/pages.go
+++ routers/pages.go
@@ -4,10 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type viewPortData struct {
-	Port string
-}
-
 func (r *Routers) ILogin() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		c.HTML(200, "login.html", nil)
@@ -16,15 +12,13 @@ func (r *Routers) ILogin() func(c *gin.Context) {
 
 func (r *Routers) IIndex() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		data := viewPortData{Port: r.Port}
-		c.HTML(200, "index.html", data)
+		c.HTML(200, "index.html", nil)
 	}
 }
 
 func (r *Routers) IApplicationToMe() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		c.HTML(200, "applicationToMe.html", gin.H{
-			"Port":         r.Port,
 			"applications": r.ApplicationToMe,
 		})
 	}
@@ -32,9 +26,7 @@ func (r *Routers) IApplicationToMe() func(c *gin.Context) {
 
 func (r *Routers) IMyApplication() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		// data := ViewData{Port: port}
 		c.HTML(200, "myapplication.html", gin.H{
-			"Port":         r.Port,
 			"applications": r.MyApplication,
 		})
 	}
